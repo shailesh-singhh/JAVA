a69ef396ce562ffec229ef68d0b6651e1c08db36
Shailesh singh <singhshailesh2901@gmail.com>
2023-09-19 19:07:08 +0530

    learning the basic fundamentals of java

---
+++ basic_topics.java
@@ -0,0 +1,10 @@
+package JAVA;
+
+public class basic_topics {
+    public static void main(String[] args){
+        System.out.println("GIVING OUTPUTS"); 
+          System.out.print("GIVING OUTPUTS IN NEXT LINE\n");
+          System.out.println("GIVING OUTPUTS");  
+    }  
+    
+}
---
+++ pattern.java
@@ -0,0 +1,30 @@
+package JAVA;
+import java.util.*;
+
+public class pattern {
+    public static void main(String[] args){
+       // System.out.println("*");
+         //System.out.println("* *");
+        //  System.out.println("* * *");                //-----CODE TO PRINT PATTERN ---//
+          // System.out.println("* * * *");
+          //---------------------------------------------------------------------------------------------------------------------------//
+      //     int a=25;
+        //  int b=10;                                  //-----------CODE TO ADD TWO NUMBERS----//
+          // int add = a + b;
+          // System.out.println(add);
+           //Scanner sc = new Scanner(System.in);
+        //   String name= sc.next();
+         //  System.out.println(name);
+    //----------------------------------------------------------------------------------------------------------------------------//
+    
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the first number :");
+    int a = sc.nextInt();
+    System.out.println("Enter the Second number :");
+    int b = sc.nextInt();                                    //--------------CODE To TAKE INPUT OF TWO VARIABLES AND PRINT THE SUM----------// 
+    int sum = a+b;
+    System.out.println(sum);
+
+        
+}
+}
