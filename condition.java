e8352180e568ba23e5b68d629e6d05894f3af0bd
Shailesh singh <singhshailesh2901@gmail.com>
2023-09-21 18:44:38 +0530

    question on coditional statement (Adult or not)

---
+++ condition.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class condition {
+
+    public static void main(String arg[]){
+        System.out.println("Enter the Age : ");
+        Scanner sc = new Scanner(System.in);
+        int age =sc.nextInt();
+        if(age>18){
+            System.out.println("ADULT");
+        }
+else{
+    System.out.println("NOT AN ADULT");
+}
+    }
+}
