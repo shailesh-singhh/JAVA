528106fde22fbe1470209e72582fee709a37f69a
Shailesh singh <singhshailesh2901@gmail.com>
2023-09-26 19:00:44 +0530

    Pyramid question

---
+++ pyramidpattern.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+public class pyramidpattern {
+    public static void main(String args[]){
+        System.out.println("Enter the number :");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1 ;j<=i;j++){
+               
+                System.out.print("*");
+            
+            }
+            System.out.println();
+        }
+    }
+}
