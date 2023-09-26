6edef0c7fbb98dc960171386c7b856f5ec7e540e
Shailesh singh <singhshailesh2901@gmail.com>
2023-09-26 18:48:37 +0530

    printing hollow rectangle

---
+++ hollowrect.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class hollowrect {
+    public static void main(String args[]){
+        System.out.println("Enter the rows:");
+        Scanner sc =new Scanner(System.in);
+         int r = sc.nextInt();
+         System.out.println("Enter the columns: ");
+         int c= sc.nextInt();
+         for(int i=1;i<=r;i++){
+            for(int j=1;j<=c;j++){
+                if(i==1 || i==r || j==1 || j==c){
+                    System.out.print("*");
+                }
+                else{
+                   System.out.print(" ");
+                }
+            }
+            System.out.println();
+         }
+        
+    }
+    
+}
